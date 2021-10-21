3b301855d42c5d2acd998d1b6ccf2dbfe4f3b4df
Gwilherm Couffy <gwilherm-couffy_student2021@wilder.school>
2021-10-21 14:39:51 +0200

    twig 03

--- public/detail.php
+++ public/detail.php
@@ -6,8 +6,8 @@ require_once __DIR__ . '/../config/twig.php';
 
 $name = 'Wilder';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('details.html.twig', ['name' => $name]);
 
 //On ajoute la globale author à l'initialisation de l'objet Twig
 $mail = 'gcouffy@gmail.com';
-$twig->addGlobal('contactEmail', $mail);
+$twig->addGlobal('mail', $mail);
